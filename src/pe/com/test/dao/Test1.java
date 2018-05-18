d1e2133793514a2f042ca6fd4001a7eb1934ec15
Cesar Quiroga (GMD-Centro de Datos) <PCQUIROGA@onp.net>
2018-05-18 15:48:47 -0500

    Tercer commit

--- src/pe/com/test/dao/Test1.java
+++ src/pe/com/test/dao/Test1.java
@@ -13,22 +13,27 @@ import pe.com.test.graphics.vehicules.Car;
 public class Test1 {
 
 	public static void main(String[] args) {
+		
+		simulation();
+	}
+	
+	public static void simulation() {
 		Track track = new Track();
 		Car car1 = new Car("car.png");
 		Car car2 = new Car("car.png");
 		Car car3 = new Car("car.png");
-		//Car car4 = new Car("car.png");
-		//Car car5 = new Car("car.png");
+		Car car4 = new Car("car.png");
+		Car car5 = new Car("car.png");
 		car1.setVelocity(5);
 		car2.setVelocity(6);
 		car3.setVelocity(10);
-		//car4.setVelocity(7);
-		//car5.setVelocity(9);
+		car4.setVelocity(7);
+		car5.setVelocity(9);
 		track.add(car1);
 		track.add(car2);
 		track.add(car3);
-		//track.add(car4);
-		//track.add(car5);
+		track.add(car4);
+		track.add(car5);
 		track.start();
 	}
 }
